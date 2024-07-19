7bbd4842e5743660f7ead449ec5a26f229006d2a
Nikolai <nick22783@gmail.com>
2024-07-19 18:36:17 +0300

    add ScorePanel

---
+++ src/components/ScorePanel/ScorePanel.tsx
@@ -0,0 +1,11 @@
+import styles from "./ScorePanel.module.css"
+
+const ScorePanel = () => {
+  return (
+    <div className={styles.ScorePanel}>
+      ScorePanel
+    </div>
+  );
+};
+
+export default ScorePanel;
