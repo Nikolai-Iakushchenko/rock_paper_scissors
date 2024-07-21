1ebf93a06a46b26f93056462b021630cc4d9edfd
Nikolai <nick22783@gmail.com>
2024-07-21 12:03:19 +0300

    adjust layout

--- src/components/BettingPositions/BettingPositionList.tsx
+++ src/components/BettingPositions/BettingPositionList.tsx
@@ -1,14 +1,18 @@
 import styles from "./BettngPositionList.module.css";
-import {ReactElement} from "react";
+import { ReactElement } from "react";
 
 interface BettingPositionListProps {
   children: ReactElement | ReactElement[];
 }
 
-const BettingPositionList = ({children}: BettingPositionListProps) => {
-  return <div className={styles.bettingPositionList}>
-    <div className={styles.bettingPositionContainer}>{children}</div>
-  </div>;
+const BettingPositionList = ({ children }: BettingPositionListProps) => {
+  return (
+    <div className={styles.bettingPositionList}>
+      {/*<div className={styles.bettingPositionContainer}>*/}
+      {children}
+      {/*</div>*/}
+    </div>
+  );
 };
 
 export default BettingPositionList;
