872dab52a7273b11635da94b3ab7657454e34301
Nikolai <nick22783@gmail.com>
2024-07-21 11:12:34 +0300

    add .bettingPositionContainer

--- src/components/BettingPositions/BettingPositionList.tsx
+++ src/components/BettingPositions/BettingPositionList.tsx
@@ -1,12 +1,14 @@
 import styles from "./BettngPositionList.module.css";
-import { ReactElement } from "react";
+import {ReactElement} from "react";
 
 interface BettingPositionListProps {
   children: ReactElement | ReactElement[];
 }
 
-const BettingPositionList = ({ children }: BettingPositionListProps) => {
-  return <div className={styles.bettingPositionList}>{children}</div>;
+const BettingPositionList = ({children}: BettingPositionListProps) => {
+  return <div className={styles.bettingPositionList}>
+    <div className={styles.bettingPositionContainer}>{children}</div>
+  </div>;
 };
 
 export default BettingPositionList;
